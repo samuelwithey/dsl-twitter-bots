e4d475957f780665c9c16543eb3739f940637763
Samuel Withey <samuelwithey@gmail.com>
2020-03-28 11:39:22 +0000

    Registered models in Admin

--- web-application/dashboard/dsl_dashboard/admin.py
+++ web-application/dashboard/dsl_dashboard/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from dsl_dashboard.models import TwitterAccount, TwitterCampaign, ScheduledPost
+
+
+class TwitterAccountAdmin(admin.ModelAdmin):
+    fields = ['username', 'consumer_key', 'consumer_secret_key', 'access_token', 'access_token_secret']
+    list_display = ['username']
+
+
+class TwitterCampaignAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'twitter_account']
+    list_display = ['name', 'description', 'twitter_account']
+
+
+class ScheduledPostAdmin(admin.ModelAdmin):
+    fields = ['campaign_name', 'status', 'scheduled_time']
+    list_display = ['campaign_name', 'status', 'scheduled_time']
+
+
+admin.site.register(TwitterAccount, TwitterAccountAdmin)
+admin.site.register(TwitterCampaign, TwitterCampaignAdmin)
+admin.site.register(ScheduledPost, ScheduledPostAdmin)
