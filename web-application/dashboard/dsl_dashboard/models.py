5f759d57d4a1d5cc52cb05d37a84ca3b1dbbd3e4
Samuel Withey <samuelwithey@gmail.com>
2020-03-28 11:52:48 +0000

    Added __str__ to models to return name

--- web-application/dashboard/dsl_dashboard/models.py
+++ web-application/dashboard/dsl_dashboard/models.py
@@ -8,14 +8,23 @@ class TwitterAccount(models.Model):
     access_token = models.CharField(max_length=200)
     access_token_secret = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.username
+
 
 class TwitterCampaign(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     twitter_account = models.ForeignKey(TwitterAccount, on_delete=models.CASCADE, related_name='twitter_account')
 
+    def __str__(self):
+        return self.name
+
 
 class ScheduledPost(models.Model):
     campaign_name = models.ForeignKey(TwitterCampaign, on_delete=models.CASCADE, related_name='campaign_name')
     status = models.CharField(max_length=240)
     scheduled_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.campaign_name
