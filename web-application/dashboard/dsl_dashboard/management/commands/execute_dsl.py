cbd5ff2cd5c523b7872fc7662dcfd4bbd5cef5c9
Samuel Withey <samuelwithey@gmail.com>
2020-05-06 12:26:07 +0100

    Implemented methods to get account and campaign through management command parameters

--- web-application/dashboard/dsl_dashboard/management/commands/execute_dsl.py
+++ web-application/dashboard/dsl_dashboard/management/commands/execute_dsl.py
@@ -1,6 +1,22 @@
 from django.core import management
 from django.core.management.base import BaseCommand
 
+from dsl_dashboard.models import TwitterAccount, TwitterCampaign
+
 
 class Command(BaseCommand):
     help = "Execute dsl commands through management commands"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--account-id', type=int)
+        parser.add_argument('--campaign-id', type=int)
+
+    def get_account(self, account_id):
+        return TwitterAccount.objects.get(id=account_id)
+
+    def get_campaign(self, account, campaign_id):
+        return TwitterCampaign.objects.get(twitter_account=account).filter(id=campaign_id)
+
+    def execute(self):
+        account = self.get_account(account_id=options['account_id'])
+        campaign = self.get_campaign(account=account, campaign_id=options['campaign_id'])
