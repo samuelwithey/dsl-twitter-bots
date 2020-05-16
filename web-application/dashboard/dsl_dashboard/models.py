fb9327044b8cbf1f2e0f079c74984d22c8607f4e
Samuel Withey <samuelwithey@gmail.com>
2020-05-16 13:06:43 +0100

    Refactored user_directory_path in models.py
    - file now uploaded to MEDIA_ROOT / user_<id>/campaign_<name>/filename

--- web-application/dashboard/dsl_dashboard/models.py
+++ web-application/dashboard/dsl_dashboard/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 def user_directory_path(instance, filename):
-    # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
-    return 'user_{0}/{1}'.format(instance.user.id, filename)
+    # file will be uploaded to MEDIA_ROOT / user_<id>/campaign_<name>/filename
+    return 'user_{0}/campaign_{1}/file/{2}'.format(instance.twitter_account.id, instance, filename)
 
 
 class TwitterAccount(models.Model):
