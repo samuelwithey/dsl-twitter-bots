de8862466af4fc785e46401e3338997dee979ae3
Samuel Withey <samuelwithey@gmail.com>
2020-05-18 20:30:01 +0100

    Implemented tweet action

--- web-application/dashboard/core/dsl/dsl_source_files/dslVisitorWalker.py
+++ web-application/dashboard/core/dsl/dsl_source_files/dslVisitorWalker.py
@@ -3,4 +3,15 @@ from .dslParser import dslParser
 
 
 class DSLVisitorWalker(dslVisitor):
-    pass
+
+    def __init__(self, tweepy_api):
+        super().__init__()
+        self.tweepy_api = tweepy_api
+
+    def visitTweet(self, ctx:dslParser.TweetContext):
+        kwargs = {}
+        kwargs[ctx.tweet_required_parameter().status().getText()] = ctx.tweet_required_parameter().value().getText()
+        for optional_param in ctx.tweet_optional_parameter():
+            kwargs[optional_param.tweet_parameter().getText()] = optional_param.value().getText()
+        self.tweepy_api.update_status(**kwargs)
+        return self.visitChildren(ctx)
