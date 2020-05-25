5d260bbcebb5265bb5bea86115e806ed3617c228
Samuel Withey <samuelwithey@gmail.com>
2020-05-25 16:56:30 +0100

    Refactored bot-scripts to bot_scripts and implemented followFollowers

---
+++ web-application/dashboard/core/bot_scripts/followFollowers.py
@@ -0,0 +1,19 @@
+import tweepy
+import logging
+import time
+
+logging.basicConfig(level=logging.INFO)
+
+
+class FollowFollowers:
+
+    def __init__(self, tweepy_api):
+        self.tweepy_api = tweepy_api
+        self.logger = logging.getLogger()
+
+    def follow_followers(self):
+        self.logger.info("Retrieving and following followers")
+        for follower in tweepy.Cursor(self.tweepy_api.followers).items():
+            if not follower.following:
+                self.logger.info(f"Following {follower.name}")
+                follower.follow()
