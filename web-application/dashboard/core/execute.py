dd32c247c18a6f4872ea60fd6fd9f8ac3363b87e
Samuel Withey <samuelwithey@gmail.com>
2020-05-25 17:20:08 +0100

    Refactored execute class
    - Seperated building the lexer and parser, building the tree and traversing the tree functionality

--- web-application/dashboard/core/execute.py
+++ web-application/dashboard/core/execute.py
@@ -3,8 +3,7 @@ from core.dsl.dsl_source_files.dslLexer import dslLexer
 from core.dsl.dsl_source_files.dslParser import dslParser
 from core.dsl.dsl_source_files.dslListener import dslListener
 from core.dsl.dsl_source_files.dslVisitorWalker import DSLVisitorWalker
-import sys
-import os
+import logging
 from antlr4 import *
 from django.conf import settings
 
@@ -14,6 +13,7 @@ class Execute:
     def __init__(self, account, campaign):
         self.account = account
         self.campaign = campaign
+        self.logger = logging.getLogger()
 
     def get_consumer_key(self, account):
         return account.consumer_key
@@ -30,23 +30,27 @@ class Execute:
     def tweepy_auth(self):
         auth = tweepy.OAuthHandler(self.get_consumer_key(self.account), self.get_consumer_secret_key(self.account))
         auth.set_access_token(self.get_access_token(account=self.account), self.get_access_token_secret(account=self.account))
-        return tweepy.API(auth)
+        api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        try:
+            api.verify_credentials()
+        except Exception as e:
+            self.logger.error("Error creating API", exc_info=True)
+            raise e
+        return api
 
     def build_lexer_parser(self):
         input_stream = FileStream(self.get_user_filename())
-        print(input_stream)
         lexer = dslLexer(input_stream)
         token_stream = CommonTokenStream(lexer)
         parser = dslParser(token_stream)
-        tree = parser.twitbot()
-        visitor = DSLVisitorWalker(self.tweepy_auth())
-        visitor.visit(tree)
-        # self.build_tree(parser=parser)
+        return parser
 
     def build_tree(self, parser):
-        tree = parser.twitbot()
-        lisp_tree_str = tree.toStringTree(recog=parser)
-        print(lisp_tree_str)
+        return parser.twitbot()
+
+    def traverse_tree(self, tree):
+        visitor = DSLVisitorWalker(self.tweepy_auth())
+        visitor.visit(tree)
 
     def get_user_filename(self):
         return settings.MEDIA_ROOT + "/" + self.campaign.upload.name
