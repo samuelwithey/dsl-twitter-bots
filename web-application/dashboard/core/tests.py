a8f62f4c8e25b7f5a58555acb8bc36350fa539e0
Samuel Withey <samuelwithey@gmail.com>
2020-05-27 17:17:35 +0100

    Implemented DSLVisitorWalkerTest
    - Implemented test_tweet function

--- web-application/dashboard/core/tests.py
+++ web-application/dashboard/core/tests.py
@@ -1,3 +1,22 @@
+import random
+import string
+
 from django.test import TestCase
 
-# Create your tests here.
+from core.execute import Execute
+
+
+class DSLVisitorWalkerTest(TestCase):
+
+    def setUp(self):
+        with open('core/api_keys.txt', 'r') as file:
+            self.data = file.read().splitlines()
+        self.consumer_key, self.consumer_secret_key, self.access_token, self.access_token_secret = [i for i in self.data]
+
+    def test_tweet(self):
+        execute = Execute(consumer_key=self.consumer_key, consumer_secret_key=self.consumer_secret_key, access_token=self.access_token, access_token_secret=self.access_token_secret)
+        api = execute.tweepy_auth()
+        input_statement = 'tweet status : "This is a Unit Test with random string: %s " ;' % ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+        parser = execute.build_lexer_parser(input_statement=input_statement)
+        tree = execute.build_tree(parser=parser)
+        execute.traverse_tree(tree=tree, api=api)
