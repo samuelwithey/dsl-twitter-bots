39088318af387a0d905251a1408f890805d41731
Samuel Withey <samuelwithey@gmail.com>
2020-05-28 10:50:10 +0100

    Refactored tests

--- web-application/dashboard/core/tests.py
+++ web-application/dashboard/core/tests.py
@@ -13,10 +13,16 @@ class DSLVisitorWalkerTest(TestCase):
             self.data = file.read().splitlines()
         self.consumer_key, self.consumer_secret_key, self.access_token, self.access_token_secret = [i for i in self.data]
 
-    def test_tweet(self):
-        execute = Execute(consumer_key=self.consumer_key, consumer_secret_key=self.consumer_secret_key, access_token=self.access_token, access_token_secret=self.access_token_secret)
+    def execute(self, input_statement):
+        execute = Execute(consumer_key=self.consumer_key, consumer_secret_key=self.consumer_secret_key,
+                          access_token=self.access_token, access_token_secret=self.access_token_secret)
         api = execute.tweepy_auth()
-        input_statement = 'tweet status : "This is a Unit Test with random string: %s " ;' % ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
         parser = execute.build_lexer_parser(input_statement=input_statement)
         tree = execute.build_tree(parser=parser)
         execute.traverse_tree(tree=tree, api=api)
+
+    def generate_random_string(self):
+        return ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+
+    def test_tweet(self):
+        self.execute(input_statement=self.generate_random_string())
