b2b3b01a81ff9878dcb7346bc27ff3989771010b
Samuel Withey <samuelwithey@gmail.com>
2020-05-30 12:42:16 +0100

    Implemented assertions to prove functionality

--- web-application/dashboard/core/tests.py
+++ web-application/dashboard/core/tests.py
@@ -16,6 +16,7 @@ class ConfigureTests(TestCase):
             self.data = file.read().splitlines()
         self.consumer_key, self.consumer_secret_key, self.access_token, self.access_token_secret = [i for i in self.data]
         self.logger = logging.getLogger()
+        self.api = self.get_tweepy_api()
 
     def execute(self, input_statement):
         execute = Execute(consumer_key=self.consumer_key, consumer_secret_key=self.consumer_secret_key,
@@ -39,21 +40,40 @@ class ConfigureTests(TestCase):
     def generate_random_string(self):
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
 
+    def get_most_recent_user_timeline_tweet(self):
+        return self.api.user_timeline(count=1)
+
+    def get_most_recent_user_mention(self):
+        return self.api.mentions_timeline(count=1)
+
+    def get_most_recent_direct_message(self):
+        return self.api.list_direct_messages(count=1)
+
 
 class DSLVisitorWalkerTest(ConfigureTests):
 
     def test_tweet(self):
-        input_statement = 'tweet status : "This is a Unit Test with random string: %s" ;' % self.generate_random_string()
+        status = 'This is a Unit Test with random string: %s' % self.generate_random_string()
+        input_statement = 'tweet status : "%s" ;' % status
         self.execute(input_statement=input_statement)
+        tweet = self.get_most_recent_user_timeline_tweet()[0]
+        self.assertEqual(tweet.text, status)
 
-    def test_tweet_optional_parameters(self):
-        input_statement = """
-        tweet 
-            status : "This is a Unit Test with random string: %s",
-            possibly_sensitive : True,
-            lat: 50.834400,
-            long: -0.130240;""" % self.generate_random_string()
+    def test_retweet(self):
+        timeline_tweet = self.get_most_recent_user_timeline_tweet()
+        self.assertFalse(timeline_tweet[0].retweeted)
+        input_statement = 'retweet id : %s ;' % timeline_tweet[0].id
         self.execute(input_statement=input_statement)
+        updated_timeline_tweet = self.get_most_recent_user_timeline_tweet()
+        self.assertTrue(updated_timeline_tweet[0].retweeted)
+
+    def test_favourite(self):
+        timeline_tweet = self.get_most_recent_user_timeline_tweet()
+        self.assertFalse(timeline_tweet[0].favorited)
+        input_statement = 'favourite id : %s ;' % timeline_tweet[0].id
+        self.execute(input_statement=input_statement)
+        updated_timeline_tweet = self.get_most_recent_user_timeline_tweet()
+        self.assertTrue(updated_timeline_tweet[0].favorited)
 
     def test_reply_to_tweet(self):
         api = self.get_tweepy_api()
@@ -63,31 +83,31 @@ class DSLVisitorWalkerTest(ConfigureTests):
         input_statement = 'reply in_reply_to_status_id : %s, status : "Hello @%s, Random Reply %s";' % (reply_id, username,
                                                                                              self.generate_random_string())
         self.execute(input_statement=input_statement)
+        timeline_tweet = self.get_most_recent_user_timeline_tweet()
+        self.assertEqual(reply_id, timeline_tweet[0].in_reply_to_status_id)
 
-    def test_retweet(self):
-        time.sleep(5)
-        api = self.get_tweepy_api()
-        timeline_tweet = api.user_timeline(count=1)
-        print(timeline_tweet)
-        tweet_id = timeline_tweet[0].id
-        input_statement = 'retweet id : %s ;' % tweet_id
+    def test_tweet_optional_parameters(self):
+        input_statement = """
+        tweet 
+            status : "This is a Unit Test with random string: %s",
+            possibly_sensitive : True,
+            lat: 50.834400,
+            long: -0.130240;""" % self.generate_random_string()
         self.execute(input_statement=input_statement)
-
-    def test_favourite(self):
-        time.sleep(5)
-        api = self.get_tweepy_api()
-        timeline_tweet = api.user_timeline(count=1)
-        print(timeline_tweet)
-        tweet_id = timeline_tweet[0].id
-        input_statement = 'favourite id : %s ;' % tweet_id
-        self.execute(input_statement=input_statement)
+        timeline_tweet = self.get_most_recent_user_timeline_tweet()
+        self.assertTrue(timeline_tweet[0].place.name, "Brighton")
 
     def test_schedule_tweet(self):
         pass
 
     def test_direct_message(self):
-        input_statement = 'direct_message recipient_id : %s, text : "Hello Friend!" ;' % '1050149543029948416'
+        recipient_id = '1050149543029948416'
+        text = 'Hello Friend! % s' % self.generate_random_string()
+        input_statement = 'direct_message recipient_id : %s, text : "%s" ;' % (recipient_id, text)
         self.execute(input_statement=input_statement)
+        direct_message = self.get_most_recent_direct_message()
+        self.assertTrue(recipient_id, direct_message[0].message_create.get('target').get('recipient_id'))
+        self.assertTrue(text, direct_message[0].message_create.get("message_data").get("text"))
 
 
 class BotScriptTest(ConfigureTests):
