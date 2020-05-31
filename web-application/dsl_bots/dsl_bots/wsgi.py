73f6fcb3aa6e0d96d66f8160c4ec370ea9b755b9
Samuel Withey <samuelwithey@gmail.com>
2020-05-31 18:27:51 +0100

    Refactored dsl-bots to dsl_bots
    - Updated all dependencies
    - Updated .gitignore file paths

--- web-application/dsl_bots/dsl_bots/wsgi.py
+++ web-application/dsl_bots/dsl_bots/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dsl-bots project.
+WSGI config for dsl_bots project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsl-bots.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsl_bots.settings')
 
 application = get_wsgi_application()
--- web-application/dsl_bots/manage.py
+++ web-application/dsl_bots/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsl-bots.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dsl_bots.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
