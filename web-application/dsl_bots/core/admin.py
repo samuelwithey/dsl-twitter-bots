c7ed06847e94cc24b1a59b30194a99ca8c4334b8
Samuel Withey <samuelwithey@gmail.com>
2020-06-18 20:31:41 +0100

    Added 'id' to list_display in admin

--- web-application/dsl_bots/core/admin.py
+++ web-application/dsl_bots/core/admin.py
@@ -5,12 +5,12 @@ from core.models import TwitterAccount, TwitterCampaign
 
 class TwitterAccountAdmin(admin.ModelAdmin):
     fields = ['username', 'consumer_key', 'consumer_secret_key', 'access_token', 'access_token_secret']
-    list_display = ['username']
+    list_display = ['id', 'username']
 
 
 class TwitterCampaignAdmin(admin.ModelAdmin):
     fields = ['name', 'description', 'twitter_account', 'dsl_program_upload', 'image_upload']
-    list_display = ['name', 'description', 'twitter_account', 'dsl_program_upload', 'image_upload']
+    list_display = ['id', 'name', 'description', 'twitter_account', 'dsl_program_upload', 'image_upload']
 
 admin.site.register(TwitterAccount, TwitterAccountAdmin)
 admin.site.register(TwitterCampaign, TwitterCampaignAdmin)
